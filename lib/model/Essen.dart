639df3c1893ef05948886c4b9c2de96c12fe6fb7
gobeitleuph <64547252+gobeitleuph@users.noreply.github.com>
2021-08-25 12:53:53 +0200

    asd

--- lib/model/Essen.dart
+++ lib/model/Essen.dart
@@ -8,9 +8,14 @@ class Essen {
   bool _vegetarisch;
 
 
-Essen(String _Name, int _Preis, bool _mitFleisch, bool _vegan, bool _vegetarisch) {
-    this._Name = _Name;
-    this._Preis = _Preis;
-    this._mitFleisch = _mitFleisch;
-    this._vegan = _vegan;
-    this._vegetarisch = _vegetarisch;
+
+
+  Essen (
+    this._Name,
+    this._Preis,
+    this._mitFleisch,
+    this._vegan,
+    this._vegetarisch,
+  );
+
+}
